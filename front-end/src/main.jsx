668c8f68dafd8c8b7ec3690414125a7886f7ad71
LeGiangne03 <gianglvhe171333@fpt.edu.vn>
2025-05-20 13:08:09 +0700

    add BrandManagement CategoryManagement ProductManagement AdminDashboard

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -4,6 +4,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'antd/dist/reset.css';
+import { ConfigProvider } from 'antd';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
