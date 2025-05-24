8bfd7cabbd091983906d163982272ac4df287233
Hoang Duy <duyuhhe171632@fpt.edu.vn>
2025-05-24 21:49:19 +0700

    update /services for App.jsx

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,14 +1,34 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { ConfigProvider, App as AntApp } from 'antd';
+import './App.css';
+import ServiceBooking from './pages/ServiceBooking/ServiceBooking';
+import ManagingService from './pages/ManagingService/ManagingService';
 import AdminDashboard from './pages/AdminDashboard';
+
+const theme = {
+  token: {
+    colorPrimary: '#d4af37',
+    colorBgContainer: '#333',
+    colorText: '#fff',
+    colorBorder: '#444',
+  },
+};
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/admin" element={<AdminDashboard />} />
-      </Routes>
-    </Router>
+    <ConfigProvider theme={theme}>
+      <AntApp>
+        <div className="app">
+          <Router>
+            <Routes>
+              <Route path="/services" element={<ServiceBooking />} />
+              <Route path="/admin" element={<AdminDashboard />} />
+            </Routes>
+          </Router>
+        </div>
+      </AntApp>
+    </ConfigProvider>
   );
 }
 
